4f699898fbb33d361b2f1d2d94695ab4bbdfae00
326eunjin <326eunjin@naver.com>
2024-01-06 16:20:55 +0900

    INITIALIZE

---
+++ src/main/java/zip/main/MainApplication.java
@@ -0,0 +1,13 @@
+package zip.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
